2854bd18afa36a9af08f233176c1d02332ea9ffc
Robin Vinck <robin.vinck@student.ehb.be>
2018-05-20 17:21:30 +0200

    Initial Commit

---
+++ testint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testint
+//
+//  Created by Robin Vinck on 20/05/18.
+//  Copyright © 2018 Robin Vinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
